ee27d026d582b893a65400bd8a7b53bad47fb669
M-Kulik <52786312+M-Kulik@users.noreply.github.com>
2019-07-31 17:05:02 +0200

    Add files via upload

--- test pracy na plikach.py
+++ test pracy na plikach.py
@@ -1,9 +1,12 @@
-import os
 import urllib.request, urllib.parse, urllib.error
 import requests
 import bs4
 from pathlib import Path, PureWindowsPath
 import html2text
+from http import cookiejar
+import json
+import tldextract
+import os
 
 ######### ZNAKI ZAKAZANE \/:*?"<>| ########
 
@@ -14,48 +17,45 @@ with open(file_dir) as fp:
     line = fp.readlines()
     line_count = len(line)
     c = 0
-# count lines for loop
+    print(line_count)
+    # count lines for loop
 
     while c < line_count:
-        url = line[c]
-        if not urllib.parse.urlparse(url).scheme:
-            print("error: invalid url (use http:// format)")
-            break
-# handle wrong url format exception
+        url_line = line[c]
+        if url_line.startswith("https://") or url_line.startswith("http://"):
+            url = url_line
+        else:
+            url = "http://"+url_line
+        # taking care of wrong url format
 
         try:
             r = urllib.request.urlopen(url)
         except urllib.error.URLError as e:
             print(e)
             break
-# handle wrong url exception
+            # handle wrong url exception
 
         soup = bs4.BeautifulSoup(r, 'html.parser')
         clean = soup.prettify()
-        html = html2text.html2text(clean)
-        print(html)
-#### html2text wspomagany beautiful soup - ominąć ciastka, menu itp. ####
+        # feeding soup with html code
 
+        no_html = html2text.html2text(clean)
+        encoded_html = no_html.encode("utf-8")
 
-        # clean_soup = soup.get_text()
-        # print(clean_soup)
+        # getting plain text from soup
 
+        domain = urllib.parse.urlparse(url)
+        netloc = domain.netloc
+        plain_url = netloc.strip()
+        file_name = plain_url.replace(".", "_")
+        print(file_name)
+        # changing url network location to proper file name
 
-        # body = clean_soup.body
-        # print(body.text)
-        # for paragraph in body.find_all('p'):
-        #     print(paragraph.text)
-        #     p_zewn = open('blablabla' + '.txt', '+w')
-        #     p_zewn.write(paragraph.text)
-
-
-
-        # print(znaczniki)
-        # tekst = znaczniki.text
-        # print(tekst)
-        # p_wyjsciowy = open(nazwa_pliku, 'w+')
-
-
-
+        os.chdir("C:\\Users\\Mateusz\\Documents\\Python programy\\Upraszczanie stron\\Pliki zewnętrzne\\scrapped")
+        text_file = open(file_name+".txt", "w+")
+        text_file.write(encoded_html.__str__())
+        text_file.close()
+        # saving text file with cleared webpage
+        print("success!")
         c += 1
-#
+
