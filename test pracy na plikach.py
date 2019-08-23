c5c1819eec4d53edd4f788af44a5a17aa846b32f
Mateusz <mateusz.kulik@gmail.com>
2019-08-23 13:27:16 +0200

    >added avoiding cookies script >more readable version of code

--- test pracy na plikach.py
+++ test pracy na plikach.py
@@ -1,10 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 import bs4
-from pathlib import Path
-import html2text
 import os
-import http
-from http import cookiejar
 
 url_list_dir = "C:/Users/Mateusz/Documents/Python programy/Upraszczanie stron/Pliki zewnętrzne/"
 url_list_file = "url_list.txt"
@@ -17,63 +13,72 @@ try:
 except FileNotFoundError:
     print("error: check urllist filename; file has to have .txt extension")
     exit()
-    # handling wrong urllist file extension
+# handling wrong urllist file extension
 
 with open(file_dir) as fp:
-    lines = [ line for line in fp.readlines() if line.strip()]
-
-    if len(lines)<3:
-        print("error: no url in line")
-        exit()
-    # handling empty lines
-
-    line_count = len(lines)
+    line = fp.readlines()
+    line_count = len(line)
     c = 0
     # counting lines for loop
 
     while c < line_count:
         scrapped_dir = os.path.join("C:/Users/Mateusz/Documents/Python programy/Upraszczanie stron/Pliki zewnętrzne/scrapped/")
+# directing to scrapped websites folder
 
-        url_line = lines[c]
+        url_line = line[c]
+        if len(url_line) < 3:
+            print("error: no url in line")
+            exit()
         if url_line.startswith("https://") or url_line.startswith("http://"):
             url = url_line
         else:
             url = "http://"+url_line
-        # taking care of wrong url format
+# taking care of wrong url format
 
         try:
             r = urllib.request.urlopen(url)
         except urllib.error.URLError as e:
             print(e)
-            # handle wrong url exception
+# handle wrong url exception
 
         search_name = url.replace(".", "_").replace("/", "-").replace(":", "^").rstrip()
-        # replacing characters incompatible with windows file saving
+# replacing characters incompatible with windows file saving format
 
         if os.path.exists(scrapped_dir + search_name + ".txt") is True:
             with open(scrapped_dir + search_name + ".txt", encoding="utf-8") as file:
                 file_is = file.read()
-                print(url + "Webpage oppened from file")
+                print(url + "Webpage opened from file")
                 c += 1
-        # printing content if present in storage folder
+# printing content if present in storage folder
 
         else:
             soup = bs4.BeautifulSoup(r, 'html.parser')
-            clean = soup.prettify()
-            # feeding soup with html code
-
-            no_html = html2text.html2text(clean)
-            # getting plain text from soup
-
-            domain = urllib.parse.urlparse(url)
-            netloc = domain.netloc
-            plain_url = netloc.strip()
-            file_name = plain_url.replace(".", "_")
-            print(url + " saved to file")
-            # changing url network location to proper file name
+            for script in soup(["script", "style"]):
+                script.extract()
+# removing scripts and hidden text
 
             with open(scrapped_dir + search_name + ".txt", "w+", encoding="utf-8") as f:
-                f.write(no_html)
-            # saving text file with cleared web page
+                raw_text = soup.get_text()
+                f.write(raw_text)
+# saving soup in raw text form
+
+                f.seek(0)
+                d = f.readlines()
+                f.seek(0)
+                for i in d:
+                    if i.find("ookie") == -1:
+                        f.write(i)
+                f.truncate()
+                f.seek(0)
+                text_no_cookies = f.read()
+                f.truncate(0)
+# taking care of cookies popouts
+
+                lines = (line.strip() for line in text_no_cookies.splitlines())
+                chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+                text = '\n'.join(chunk for chunk in chunks if chunk)
+                f.write(text)
+                print(text)
+# converting text to more readable form
 
             c += 1
