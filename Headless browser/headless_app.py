89bd5edaff1e916375b6300a47b39590ae370b54
M-Kulik <52786312+M-Kulik@users.noreply.github.com>
2019-11-18 14:51:53 +0100

    rearanged layout of server paths, checked different solutions - no result

--- Headless browser/headless_app.py
+++ Headless browser/headless_app.py
@@ -6,37 +6,46 @@ import time
 from flask import Flask
 import html2text
 
-url = urllib.request.urlopen('http://127.0.0.1:5000/website')
-# off = open('random_website.html', 'r', encoding='utf-8')
-soup = bs4.BeautifulSoup(url, 'lxml-xml', from_encoding='utf-8')
+adress = r'http://127.0.0.1:5000/website'
+url = urllib.request.urlopen(adress)
 
+soup = bs4.BeautifulSoup(url, 'lxml-xml')
+
+for script in soup(["script", "style"]):
+    script.extract()
+
+# contents of body
 tags = []
-
 for kid in soup.body.contents:
     if kid.name is not None:
         tags.append(kid)
 
+# TODO:
+#   1. There is no spectfic class/id for all text sections,
+#   2. Text sections ar not on the same tree level,
+#   3. Text section tends to be in the same tag as side bars,
+#   4. DOM elements have no one size format
 
+# works with first child
+a = max(tags, key=len).text
+print(a)
+exit()
+tags2 = []
+
+
+print(tags2)
 test = []
 for tagg in tags:
     test.append(list([line for line in tagg.strings]))
 hi = max(test, key=len)
-print(hi)
+print(test)
 
+hihi = max(hi, key=len)
 
-#  tree traverse #1
-# first_run = soup.body.children
-# for kid in first_run:
-#     if kid.name is not None:
-#         for kid2 in kid.children:
-#             print(kid2)
-# root_childs = [e.name for e in first_run if e.name is not None]
-# body_divs = [soup.body.child for e in soup.body.child if e.name is not None]
-# # for e in soup.body.children:
-# #     body_divs.append(e)
-#
-# print(body_divs)
-# print(root_childs)
-# root_childs2 = []
-# for child in root_childs:
-#     print(child)
+    # SELENIUM WEBDRIVER
+# options = webdriver.ChromeOptions()
+# driver = webdriver.Chrome()
+# driver.get(adress)
+# content = driver.page_source
+# options.add_argument('--headless')
+# options.add_argument('disable-gpu')
--- Headless browser/server.py
+++ Headless browser/server.py
@@ -1,9 +1,36 @@
 from flask import Flask
 from flask import render_template
+import bs4
+import urllib
+import urllib.request
 
 app = Flask(__name__)
 
 
 @app.route('/website')
 def website():
-    return render_template('website.html')
+    return render_template('wiki.html')
+
+
+@app.route('/<path:url>', methods=['GET'])
+def getfile(url):
+
+    r = urllib.request.urlopen(url)
+    soup = bs4.BeautifulSoup(r, 'lxml-xml')
+
+    for script in soup(["script", "style"]):
+        script.extract()
+
+    # contents of body
+    tags = []
+    for kid in soup.body.contents:
+        if kid.name is not None:
+            tags.append(kid)
+
+    # TODO: get children of max[tags],
+    #  put them in separate lists in one list,
+    #  then get the longest list,
+    #  work on these tags
+
+    a = max(tags, key=len).text
+    return a
