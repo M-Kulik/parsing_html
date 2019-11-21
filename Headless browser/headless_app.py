c3ac1e4111b90820b8cb5449616e16ca94d385ad
M-Kulik <52786312+M-Kulik@users.noreply.github.com>
2019-11-21 09:48:01 +0100

    added fask web functionality, urllib raise exception

--- Headless browser/headless_app.py
+++ Headless browser/headless_app.py
@@ -1,34 +1,38 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import bs4
 import urllib.request, urllib.error
-import time
 from flask import Flask
-import html2text
 
-adress = r'https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python'
-url = urllib.request.urlopen(adress)
+app = Flask(__name__)
 
-soup = bs4.BeautifulSoup(url, 'html.parser')
-for script in soup(["script", "style"]):
-    script.extract()
 
-# getting all <p> tags
-tags = []
-for tag in soup.find_all('p', class_=False):
-    tags.append(tag)
+@app.route('/<path:url>', methods=['GET'])
+def getfile(url):
 
-# getting all <p> tags parents
-ptags = []
-for ptag in tags:
-    if ptag.parent is not None:
-        if ptag.parent not in ptags:
-            ptags.append(ptag.parent)
+    content = urllib.request.urlopen(url)
+    soup = bs4.BeautifulSoup(content, 'lxml')
+    # for script in soup(["script", "style"]):
+    #     script.extract()
 
-for tagt in ptags:
-    print(tagt.text)
+    # getting all <p> tags
+    tags = []
+    for tag in soup.find_all('p', class_=False):
+        tags.append(tag)
 
-exit()
+    # getting all <p> tags parents
+    ptags = []
+    for ptag in tags:
+        if ptag.parent is not None:
+            if ptag.parent not in ptags:
+                ptags.append(ptag.parent)
+
+    for tagt in ptags:
+        return tagt.text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 # TODO:
 #   PROBLEMS
 #   1. There is no spectfic class/id for all text sections,
@@ -42,28 +46,30 @@ exit()
 #   1. Finding body by css size:
 #        -by padding
 #        -by px, %, vh;vw
+#
+#   getting all <p> tags and returning thier parents worked
 
-
-# works with first child
-a = max(tags, key=len).text
-print(a)
-exit()
-tags2 = []
-
-
-print(tags2)
-test = []
-for tagg in tags:
-    test.append(list([line for line in tagg.strings]))
-hi = max(test, key=len)
-print(test)
-
-hihi = max(hi, key=len)
-
+#
+# # works with first child
+# a = max(tags, key=len).text
+# print(a)
+# exit()
+# tags2 = []
+#
+#
+# print(tags2)
+# test = []
+# for tagg in tags:
+#     test.append(list([line for line in tagg.strings]))
+# hi = max(test, key=len)
+# print(test)
+#
+# hihi = max(hi, key=len)
+#
     # SELENIUM WEBDRIVER
-# options = webdriver.ChromeOptions()
-# driver = webdriver.Chrome()
-# driver.get(adress)
-# content = driver.page_source
-# options.add_argument('--headless')
-# options.add_argument('disable-gpu')
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # options.add_argument('disable-gpu')
+    # driver = webdriver.Chrome(options=options)
+    # driver.get(adress)
+    # content = driver.page_source
