87860ea5120c9e95eed8ef5a1e8343455c653df9
M-Kulik <52786312+M-Kulik@users.noreply.github.com>
2019-11-26 12:19:13 +0100

    cleaned

--- Headless browser/headless_app.py
+++ Headless browser/headless_app.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-<<<<<<< selenium
 import re
 
 # SELENIUM WEBDRIVER
@@ -40,155 +39,12 @@ for elem in element:
     if elem.text == maxlen:
         lentest.append(elem)
 
-
-# SELENIUM WEBDRIVER
-url = 'https://www.crummy.com/software/BeautifulSoup/bs4/doc/#replace-with'
->>>>>>> local
-
-async def main():
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto('https://miyakogi.github.io/pyppeteer/index.html')
-    await page.screenshot({'path': r'C:\Users\Mateusz\test.png'})
-    await browser.close()
-
-asyncio.get_event_loop().run_until_complete(main())
-
-# # list of all elements
-# element = driver.find_elements_by_xpath('//*[@id]')
-#
-# # making list of all elements with max width
-# domlist = []
-# longest = lentest[0].size
-# for dom in element:
-#     doms = dom.size
-#     if doms.get('width') == longest.get('width'):
-#         if dom.text not in domlist:
-#             domlist.append(dom)
-
-
-<<<<<<< selenium
-# # # TODO: get width of element with most text, get all elements with same width
-#
-# # getting size width atribute of all dom elements
-# size = [s.size for s in element]
-# a = [x.get('width') for x in size]
-# maxsize = max(a)
-#
-# # making list of all elements with max width
-# domlist = []
-# for dom in element:
-#     doms = dom.size
-#     if doms.get('width') == maxsize:
-#         domlist.append(dom)
-#
-#
-#
-#
-# exit()
-# e = element[0].size
-# print(e.get('width'))
-# exit()
-#
-# print(test)
-# exit()
-# print(max([elem.size for elem in element.value]))
-# exit()
-# elelist = []
-#
-# for ele in element:
-#     elelist.append(str(ele.get_property('width')))
-#                    # .split("'width': ")[1].split('}')[0])
-# print(elelist)
-# widest = driver.find_elements_by_id('513')
-# print(widest)
-# exit()
-# with open('elementlist.txt', 'w+') as f:
-#     for ele in element:
-#         f.write(str(ele.size))
-#         f.write('\n')
-# exit()
-# # content = requests.get(url)
-# soup = bs4.BeautifulSoup(content, 'html.parser')
-# # for script in soup(["script", "style"]):
-# #     script.extract()
-#
-# # getting all <p> tags
-# tags = []
-# for tag in soup.find_all('p', class_=False):
-#     tags.append(tag)
-#
-# # getting all <p> tags parents
-# ptags = []
-# for ptag in tags:
-#     if ptag.parent is not None:
-#         if ptag.parent not in ptags:
-#             ptags.append(ptag.parent)
-#
-# for tagt in ptags:
-#     print(tagt.text)
-#
-# # TODO:
-# #   PROBLEMS
-# #   1. There is no spectfic class/id for all text sections,
-# #   2. Text sections ar not on the same tree level,
-# #   3. Text section tends to be in the same tag as side bars,
-# #   4. DOM elements have no one size format,
-# #   5. Text sections tends to be in biger DOM elements - can't chose section by size
-# #   6. bs4 finds all p tags, separated parents
-# #   7. Can't search for parent with most <p> tags, different parent tag division
-# #   TRY
-# #   1. Finding body by css size:
-# #        -by padding
-# #        -by px, %, vh;vw
-# #
-# #   getting all <p> tags and returning thier parents worked
-#
-# #
-# # # works with first child
-# # a = max(tags, key=len).text
-# # print(a)
-# # exit()
-# # tags2 = []
-# #
-# #
-# # print(tags2)
-# # test = []
-# # for tagg in tags:
-# #     test.append(list([line for line in tagg.strings]))
-# # hi = max(test, key=len)
-# # print(test)
-# #
-# # hihi = max(hi, key=len)
-# #
-#     # SELENIUM WEBDRIVER
-#     # options = webdriver.ChromeOptions()
-#     # options.add_argument('--headless')
-#     # options.add_argument('disable-gpu')
-#     # driver = webdriver.Chrome(options=options)
-#     # driver.get(adress)
-#     # content = driver.page_source
-# #
-# # url = 'https://www.crummy.com/software/BeautifulSoup/bs4/doc/#replace-with'
-# #
-# # content = requests.get(url)
-# # soup = bs4.BeautifulSoup(content.content, 'lxml')
-# # # for script in soup(["script", "style"]):
-# # #     script.extract()
-# #
-# # # getting all <p> tags
-# # tags = []
-# # for tag in soup.find_all('p', class_=False):
-# #     tags.append(tag)
-# #
-# # # getting all <p> tags parents
-# # ptags = []
-# # for ptag in tags:
-# #     if ptag.parent is not None:
-# #         if ptag.parent not in ptags:
-# #             ptags.append(ptag.parent)
-# #
-# # for tagt in ptags:
-# #     print(tagt.text)
-=======
->>>>>>> local
+# making list of all elements with max width
+domlist = []
+longest = lentest[0].size
+for dom in element:
+    doms = dom.size
+    if doms.get('width') == longest.get('width'):
+        if dom.text not in domlist:
+            domlist.append(dom)
+            print(dom.text)
